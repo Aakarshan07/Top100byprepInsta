e1de45b1a4d96a9d5bf353a9802c774802780db3
Aakarshan Bhardwaj <aakarshanbhardwaj07@gmail.com>
2022-07-19 16:13:29 +0530

    Add files via upload

---
+++ strlen.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        count++;
+    }
+    cout<<str.length()<<" is same as "<<count;
+}
