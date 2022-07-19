2667ee055601ab3654d528b903703b9725cf5249
Aakarshan Bhardwaj <aakarshanbhardwaj07@gmail.com>
2022-07-19 16:13:19 +0530

    Add files via upload

---
+++ toggle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    for(int i=0;str[i]!='\0';i++){
+        if(isupper(str[i])){
+            str[i]=tolower(str[i]);
+        }
+        else if(islower(str[i])){
+            str[i]=toupper(str[i]);
+        }
+    }
+    cout<<str;
+}
