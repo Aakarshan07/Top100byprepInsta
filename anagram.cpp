f0ea78da8bf7f6de2740dbb2021006c170882c03
Aakarshan Bhardwaj <aakarshanbhardwaj07@gmail.com>
2022-08-01 18:23:48 +0530

    Add files via upload

--- anagram.cpp
+++ anagram.cpp
@@ -5,18 +5,18 @@ int main(){
     getline(cin,str1);
     getline(cin,str2);
     map<char,int> mp;
-    for(int i=0;str1[i]!='\0';i++){
+    for(int i=0;i<str1.length();i++){
         mp[str1[i]]++;
     }
-    for(int i=0;str2[i]!='\0';i++){
-        mp[str2[i]]--;
+    for(int i=0;i<str2.length();i++){
+        mp[str2[i]]++;
     }
-    for(auto itr: mp){
-        if(itr.second!=0){
-            cout<<"not anagram";
+    for(auto itr :mp){
+        if(itr.second==1){
+            cout<<"not anagram"<<" ";
             return 0;
         }
     }
-    cout<<"anagram";
-    return 1;
+    cout<<"anagram"<<" ";
+    return 0;
 }
